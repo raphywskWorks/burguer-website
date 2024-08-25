c64f4e45a9175ac0633d7eb60b2e4ab2706f699c
Raphael Dinelli <raphael.dinelli14@gmail.com>
2024-08-25 19:54:52 -0300

    feat(header) Create logic to load custom scripts and styles

--- app/src/templates/header.php
+++ app/src/templates/header.php
@@ -6,11 +6,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="<?= '../../assets/css/pages/' . \App\shared\Helpers::getPageSlug() . '.css' ?>">
+
+    <?php if ( isset($args['stylesheets']) ): ?>
 
 
-    <title><?= isset($args) ? $args['page-title'] : 'Title' ?></title>
+        <?php foreach($args['stylesheets'] as $key => $style): ?>
+
+            <link rel="stylesheet" href="../../assets/css/<?= $style . '.css'  ?>"></link>
+
+        <?php endforeach; ?>
+
+    <?php endif; ?>
+
+
+    <title><?= $args['page-title'] ?? 'Title' ?></title>
 </head>
 <body>
 
-</body>
-</html>
+<?php
+    if ( isset($args['header-template']) ) {
+        include __dir__ . "/" . $args['header-template'] . ".php";
+    }
+
+?>
